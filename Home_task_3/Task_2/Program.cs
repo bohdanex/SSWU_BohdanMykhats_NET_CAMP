1b42f516b907bdb8d7ff644f9cf32f2b462d49c2
Bohdan <bodiamykhats@gmail.com>
2023-04-03 22:58:20 +0300

    UML and work with strings

---
+++ Home_task_3/Task_2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace WorkWithStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int? secondOccurence= null;
+                int upperCaseCount = 0;
+                string? enteredText = null;
+
+                Console.WriteLine("\nThis app do the following operations with text:" +
+                    "\n 1. Finds the index of the second occurrence of the given substring in the text;" +
+                    "\n 2. Returns the number of Uppercase words in the text;" +
+                    "\n 3. Replace all words containing doubling of letters with the given one.");
+
+                while (true)
+                {
+                    Console.Write("\nPlease enter a text\n -> ");
+                    enteredText = Console.ReadLine();
+
+                    if (enteredText == null || string.IsNullOrEmpty(enteredText)) continue;
+
+                    break;
+                }
+
+                upperCaseCount = WorkWithStrings.CountWordsStartingWithUpperCase(enteredText);
+                Console.WriteLine("\nUppercase word count: " + upperCaseCount);
+
+                Console.Write("\nPlease enter a substring for the first option -> ");
+                secondOccurence = WorkWithStrings.GetSecondOccurenceIndex(enteredText, Console.ReadLine());
+                Console.WriteLine("The index of the second occurrence is " + secondOccurence);
+
+                Console.Write("\nPlease enter a substring to be replaced with doubings: -> ");
+                Console.WriteLine("Result: " + WorkWithStrings.ReplaceRepeatingCharacters(enteredText, Console.ReadLine()));
+
+                ConsoleKey consoleKey;
+                do
+                {
+                    Console.WriteLine("\nDo you want to run this again? press Y/N -> ");
+                    consoleKey = Console.ReadKey(true).Key;
+                }
+                while (consoleKey != ConsoleKey.Y && consoleKey != ConsoleKey.N);
+
+                if (consoleKey == ConsoleKey.N) break;
+            }
+        }
+    }
+}
---
+++ Home_task_3/Task_2/WorkWithStrings.cs
@@ -0,0 +1,86 @@
+﻿namespace WorkWithStrings
+{
+    public class WorkWithStrings
+    {
+        public string Text { get; set; }
+
+        public WorkWithStrings(string text)
+        {
+            Text = text;
+        }
+
+        public void ChangeText(string text)
+        {
+            Text = text;
+        }
+
+        //Цей клас має статичні методи. Тому для того, щоб не повторювати код,
+        //методи екземплярів будуть викликати статичні методи
+        public int? GetSecondOccurenceIndex(string occurrenceText)
+        {
+            return GetSecondOccurenceIndex(Text, occurrenceText);
+        }
+
+        public int CountWordsStartingWithUpperCase()
+        {
+            return CountWordsStartingWithUpperCase(Text);
+        }
+
+        public string ReplaceRepeatingCharacters(string replaceWith)
+        {
+            return ReplaceRepeatingCharacters(Text, replaceWith);
+        }
+
+        public static int? GetSecondOccurenceIndex(string text, string occurrenceText)
+        {
+            int? occurenceIndex = text.IndexOf(occurrenceText);
+            occurenceIndex = text.IndexOf(occurrenceText, occurrenceText.Length + occurenceIndex.Value);
+
+            if (occurenceIndex.Value == -1) return null;
+
+            return occurenceIndex;
+        }
+
+        public static int CountWordsStartingWithUpperCase(string text)
+        {
+            int uppercaseCount = 0;
+            char[] splitChars = {' ',',','.','!','?'};
+            string[] textToFind = text.Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string word in textToFind)
+            {
+                if (char.IsUpper(word[0])) uppercaseCount++;
+            }
+
+            return uppercaseCount;
+        }
+
+        public static string ReplaceRepeatingCharacters(string text, string replaceWith)
+        {
+            char[] splitChars = { ' ', ',', '.', '!', '?' };
+            string[] textToFind = text.Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
+
+            for(int wordIndex = 0; wordIndex < textToFind.Length; ++wordIndex)
+            {
+                string currentWord = textToFind[wordIndex];
+
+                for (int letterIndex = 1; letterIndex < currentWord.Length; ++letterIndex)
+                {
+                    if (currentWord[letterIndex] == currentWord[letterIndex - 1])
+                    {
+                        textToFind[wordIndex] = replaceWith;
+                        break;
+                    }
+                }
+            }
+
+            string output = string.Join(" ", textToFind);
+            return output;
+        }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
